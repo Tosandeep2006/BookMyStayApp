7546258b1bf537cdbfc2b19fc28c5c5765055ef3
Tosandeep2006 <sandeepgiridhar02@gmail.com>
2026-03-10 10:05:20 +0530

    UC1: Add welcome message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+
+        System.out.println("Welocme To the Hotel Booking Management System.");
+
+        System.out.println("System Initialized Successfully");
+    }
+}
